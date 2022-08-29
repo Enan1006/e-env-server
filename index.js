aeca386714d032965d3a9ff59719b53702a6a6e1
Imdadul Haque Enan <ihenan1006@gmail.com>
2022-08-29 12:14:41 +0600

    put and delete endpoint for inventory items

--- index.js
+++ index.js
@@ -34,7 +34,64 @@ async function run() {
             res.send(result)
         });
 
+        app.get('/inventory/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await carsCollection.findOne(query);
+            res.send(result)
+        });
 
+        app.post('/inventories', async (req, res) => {
+            const data = req.body;
+            const query = {
+                name: data.name,
+                description: data.description,
+                price: data.price,
+                quantity: data.quantity,
+                supplier: data.supplier,
+                image: data.image,
+                user: data.user
+            };
+            const result = await carsCollection.insertOne(query);
+            res.send(result)
+        });
+
+        app.get('/inventory', async (req, res) => {
+            const email = req.query.email;
+            const query = { user: email };
+            const cursor = carsCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result)
+        });
+
+        app.delete('/inventory/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log(id);
+            const query = { _id: ObjectId(id) };
+            const result = await carsCollection.deleteOne(query);
+            if (result.deletedCount === 1) {
+                console.log("Deleted Successfully");
+            }
+            else {
+                console.log("Unsuccessful attempt")
+            }
+        });
+
+        app.put('/inventory/:id', async (req, res) => {
+            const id = req.params.id;
+            const quantity = req.body.quantity;
+            console.log(quantity);
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: {
+                    quantity: quantity
+                }
+            }
+            const result = await carsCollection.updateOne(filter, updateDoc, options);
+            console.log(result);
+            res.send(result);
+        })
     }
     finally {
 
